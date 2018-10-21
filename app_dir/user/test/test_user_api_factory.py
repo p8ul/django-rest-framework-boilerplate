93b143bcd352220c750d75886fa5d1bf435db284
p8ul <pkinuthia10@gmail.com>
2018-10-21 05:55:11 +0300

    Add listing and update user tests

--- app_dir/user/test/test_user_api_factory.py
+++ app_dir/user/test/test_user_api_factory.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIClient
+from faker import Factory
 from app_dir.factories import UserFactory
+
+faker = Factory.create()
 
 
 class UserTest(TestCase):
@@ -9,15 +12,26 @@ class UserTest(TestCase):
         self.user = UserFactory()
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
+
         self.namespace = 'user_api'
+        self.body = {
+            'username': faker.first_name(),
+            'email': faker.email(),
+            'password': faker.password()
+        }
         self.create_url = reverse(self.namespace + ':user-creator')
         self.list_url = reverse(self.namespace + ':user-list')
-        # self.update_url = reverse(self.namespace + ':user-updater')
+        self.update_url = reverse(self.namespace + ':user-updater', kwargs={'pk': self.user.id})
 
-    def test_list_factory(self):
-        response = self.client.post(self.create_url, {}, format='json')
-        self.assertEqual(400, response.status_code)
+    def test_create_user_api(self):
+        response = self.client.post(self.create_url, self.body, format='json')
+        self.assertEqual(201, response.status_code)
 
-    def test_listing_created_user(self):
+    def test_listing_user_api(self):
         response = self.client.get(self.list_url)
         self.assertContains(response, self.user)
+
+    def test_update_user_api(self):
+        response = self.client.put(self.update_url, self.body, format='json')
+        self.assertEqual(200, response.status_code)
+
